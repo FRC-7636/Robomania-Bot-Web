86d33052d71b6e693edd2620bac831e9cd0c292c
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-08-10 14:41:07 +0800

    feat(meeting): 完成 `/meeting/<id>/edit` 頁面
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Meetings/views.py
+++ Meetings/views.py
@@ -1,8 +1,11 @@
 # coding=utf-8
 from django.contrib.auth.decorators import login_required, permission_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+import datetime
+from zoneinfo import ZoneInfo
 
 from .models import DMeeting
+from Members.models import DMember
 
 
 # Create your views here.
@@ -11,6 +14,8 @@ def index(request, meeting_id):
     meeting = get_object_or_404(DMeeting, pk=meeting_id)
     return render(request, 'Meetings/index.html',
                   {"meeting": meeting,
+                   "meeting_description": meeting.description.replace("`",
+                                                                      "\\`") if meeting.description is not None else "",
                    "can_edit": request.user.has_perm("Meetings.change_DMeeting") and request.user.has_perm(
                        "Meetings.delete_DMeeting")
                    })
@@ -19,4 +24,36 @@ def index(request, meeting_id):
 @permission_required(["Meetings.change_DMeeting", "Meetings.delete_DMeeting"], raise_exception=True)
 def edit(request, meeting_id):
     meeting = get_object_or_404(DMeeting, pk=meeting_id)
-    return render(request, 'Meetings/edit.html', {"meeting": meeting})
+    if request.method == "POST":
+        taipei_tz = ZoneInfo("Asia/Taipei")
+        # update meeting
+        meeting.title = request.POST.get("title", "")
+        meeting.description = request.POST.get("description", "")
+        meeting.host = DMember.objects.get(discord_id=int(request.POST.get("host", "")))
+        meeting.start_time = datetime.datetime.fromisoformat(request.POST.get("start-time")).astimezone(taipei_tz)
+        if request.POST.get("end-time", None):
+            meeting.end_time = datetime.datetime.fromisoformat(request.POST.get("end-time")).astimezone(taipei_tz)
+        else:
+            meeting.end_time = None
+        meeting.location = request.POST.get("location", "")
+        meeting.can_absent = request.POST.get("can-absent", "False").lower() == "true"
+        # save changes
+        meeting.save()
+        return redirect("meeting_info", meeting_id=meeting_id)
+    elif request.method == "DELETE":
+        # delete meeting
+        meeting.delete()
+        return redirect("index")
+    else:  # GET
+        # generate member choices for host selector
+        all_members = DMember.objects.all()
+        member_choices = []
+        for member in all_members:
+            member_choices.append(
+                {"discord_id": member.discord_id, "real_name": member.real_name, "avatar": member.avatar}
+            )
+        return render(request, 'Meetings/edit.html',
+                      {"meeting": meeting,
+                       "meeting_description":
+                           meeting.description.replace("`", "\\`") if meeting.description is not None else "",
+                       "member_list": member_choices})
---
+++ static/js/Meetings/edit.js
@@ -0,0 +1,69 @@
+function checkTime() {
+    const rawStartTime = document.getElementById("start-time-selector").value;
+    const rawEndTime = document.getElementById("end-time-selector").value;
+    if (rawStartTime === "" || rawEndTime === "") {
+        return; // No end time selected, no need to check
+    }
+    const startTime = new Date(rawStartTime).getTime();
+    const endTime = new Date(rawEndTime).getTime();
+
+    const startTimeText = document.getElementById("start-time-text");
+    const endTimeText = document.getElementById("end-time-text");
+    if (endTime <= startTime) {
+        startTimeText.error = true;
+        startTimeText.errorText = "結束時間需在開始時間之後。";
+        endTimeText.error = true;
+        endTimeText.errorText = "結束時間需在開始時間之後。";
+    } else {
+        startTimeText.error = false;
+        startTimeText.errorText = "";
+        endTimeText.error = false;
+        endTimeText.errorText = "";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("start-time-selector").addEventListener("change", function(event) {
+        checkTime();
+        const startTimeText = document.getElementById("start-time-text");
+        if (event.target.value === "") {
+            startTimeText.value = "";
+        } else {
+            const startTime = new Date(event.target.value);
+
+            const year = startTime.getFullYear();
+            const month = (startTime.getMonth() + 1).toString().padStart(2, "0");
+            const day = startTime.getDate().toString().padStart(2, "0");
+            const hours = startTime.getHours().toString().padStart(2, "0");
+            const minutes = startTime.getMinutes().toString().padStart(2, "0");
+
+            startTimeText.value = `${year}/${month}/${day} ${hours}:${minutes}`;
+        }
+    })
+
+    document.getElementById("end-time-selector").addEventListener("change", function(event) {
+        checkTime();
+        const endTimeText = document.getElementById("end-time-text");
+        if (event.target.value === "") {
+            endTimeText.value = "";
+        } else {
+            const endTime = new Date(event.target.value);
+
+            const year = endTime.getFullYear();
+            const month = (endTime.getMonth() + 1).toString().padStart(2, "0");
+            const day = endTime.getDate().toString().padStart(2, "0");
+            const hours = endTime.getHours().toString().padStart(2, "0");
+            const minutes = endTime.getMinutes().toString().padStart(2, "0");
+
+            endTimeText.value = `${year}/${month}/${day} ${hours}:${minutes}`;
+        }
+    })
+
+    document.getElementById("can-absent-switch").addEventListener("change", function(event) {
+        document.getElementById("can-absent-bool").value = event.target.selected;
+    })
+
+    document.getElementById("submit-button").addEventListener("click", function() {
+        document.getElementById("meeting-form").submit();
+    })
+})
