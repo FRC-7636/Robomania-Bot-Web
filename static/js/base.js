b254daa7f145b5b98bf9ec8ade9a54e9dc84c683
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-08-10 13:09:33 +0800

    feat(base): 新增 `header` 的收合功能
    `/meeting/<id>/edit/` 的說明編輯器以全螢幕展開時，工具列會遭到 `header` 阻擋，此功能可避免此問題
    
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- static/js/base.js
+++ static/js/base.js
@@ -10,4 +10,17 @@ document.addEventListener("DOMContentLoaded", function(e) {
             window.location.href = "/accounts/login";
         })
     }
+
+    document.getElementById("collapse-btn").addEventListener("click", function (e) {
+        if (e.target.classList.contains("collapsed")) {
+            e.target.classList.remove("collapsed");
+            e.target.textContent = "收合 ↑";
+            document.getElementsByTagName("header")[0].classList.remove("collapsed");
+        }
+        else {
+            e.target.classList.add("collapsed");
+            e.target.textContent = "展開 ↓";
+            document.getElementsByTagName("header")[0].classList.add("collapsed");
+        }
+    })
 })
