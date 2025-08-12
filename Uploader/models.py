fc887d903f531cbf903c63876fece7fc18366ed5
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-08-12 15:12:22 +0800

    fix(uploader): 修正「`UserFile` 中的 `file_path` 函式導致無法合併資料庫」的問題
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Uploader/models.py
+++ Uploader/models.py
@@ -4,6 +4,14 @@ from django.forms import ModelForm
 from uuid import uuid4
 
 
+def file_path(instance, filename):
+    """
+    Generate a file path for the uploaded file.
+    The path will be 'uploader/<uuid>/<filename>'.
+    """
+    return f"uploader/{instance.uuid}"
+
+
 # Create your models here.
 class UserFile(models.Model):
     class Meta:
@@ -13,14 +21,6 @@ class UserFile(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.mimetype})"
-
-    @staticmethod
-    def file_path(instance, filename):
-        """
-        Generate a file path for the uploaded file.
-        The path will be 'uploader/<uuid>/<filename>'.
-        """
-        return f'uploader/{instance.uuid}'
 
     file = models.FileField("檔案物件", upload_to=file_path, max_length=300)
     uuid = models.UUIDField("UUID", default=uuid4, editable=False, unique=True)
