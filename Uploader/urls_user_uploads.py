35b11285ab76df847c643febbef72529731ba964
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-08-12 15:11:25 +0800

    fix(uploader): 修正「`/user_uploads/uploader/<uuid>` 有多餘斜線」的問題
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Uploader/urls_user_uploads.py
+++ Uploader/urls_user_uploads.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/uploader/<str:uuid>", views.download, name='download'),
+    path("uploader/<str:uuid>", views.download, name='download'),
 ]
