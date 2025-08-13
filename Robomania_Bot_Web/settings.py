8905ad2ba4b6a1cac4b35238dd36fe2d531cf33c
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-08-13 19:02:01 +0800

    feat(settings): `DEBUG` 變數改以環境變數讀取
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Robomania_Bot_Web/settings.py
+++ Robomania_Bot_Web/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = []
 
