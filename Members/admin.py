15064c1b8921eb1a11f68d350275726d19836210
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-08-17 19:59:26 +0800

    feat(member): 新增 `/member/<id>` 頁面及 `WarningHistory` 模型
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Members/admin.py
+++ Members/admin.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from django.contrib import admin
-from .models import DMember
+from .models import DMember, WarningHistory
 
 # Register your models here.
 admin.site.register(DMember)
+admin.site.register(WarningHistory)
--- Members/views.py
+++ Members/views.py
@@ -9,8 +9,13 @@ from .models import DMember
 @login_required
 def info(request, member_id):
     member = get_object_or_404(DMember, discord_id=member_id)
+    data = {"member": member,
+            "warning_history": member.warning_history.all(),
+            "is_self": member.discord_id == request.user.discord_id,
+            "can_edit": request.user.has_perm("Members.change_DMember") or member.discord_id == request.user.discord_id,
+            "can_edit_warning_points": request.user.has_perms(["Members.change_DMember", "Members.add_WarningHistory"])}
     return render(
         request,
-        'Members/info.html',
-        {"member_info": member}
+        'Members/index.html',
+        data
     )
