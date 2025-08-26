5e3cf1a0ed765349967cd1c339f7fd7d6920743f
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-08-26 17:51:24 +0800

    fix(panel): 修正「記點紀錄在視窗尺寸變更時，高度會超出容器範圍」的問題
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- static/js/Panel/index.js
+++ static/js/Panel/index.js
@@ -5,5 +5,16 @@ function measureHeight() {
     document.getElementById("warning-history-container").style.height = `${height}px`;
 }
 
+function resizeWarnStatus() {
+    const warnStatusText = document.getElementById("warn-status-text");
+    const containerWidth = document.getElementById("upcoming-meetings-list-div").offsetWidth;
+    const em = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+    warnStatusText.style.width = `${containerWidth - (1 + 0.25) * em - 24}px`;
+    console.log(`${containerWidth - (1 + 0.25) * em - 24}px`)
+}
+
 window.addEventListener("resize", measureHeight);
+window.addEventListener("resize", resizeWarnStatus);
 window.addEventListener("load", measureHeight);
+window.addEventListener("load", resizeWarnStatus);
