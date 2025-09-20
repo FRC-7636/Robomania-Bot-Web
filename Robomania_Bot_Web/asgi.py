4caa691f429a3bd148c5a1f07414c5882c43d542
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-09-20 11:13:50 +0800

    fix(settings): 修正「Daphne 啟動時會回報 `ImproperlyConfigured` 錯誤」的問題
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Robomania_Bot_Web/asgi.py
+++ Robomania_Bot_Web/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -17,6 +18,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 
 # Set setting modules before importing routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Robomania_Bot_Web.settings')
+django.setup()
 
 import Meetings.routing
 import Members.routing
