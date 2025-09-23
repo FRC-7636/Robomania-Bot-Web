2157c208404e0b67db630717ba527fff51532d11
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-09-23 22:15:05 +0800

    feat(announcement): 在首頁面板中新增公告跑馬燈
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Panel/views.py
+++ Panel/views.py
@@ -2,16 +2,52 @@
 from django.shortcuts import render
 import datetime
 from zoneinfo import ZoneInfo
+from markdown import Markdown
+from io import StringIO
 
+from Announcements.models import Announcement
 from Meetings.models import DMeeting
 
 
 TAIPEI_TZ = ZoneInfo("Asia/Taipei")
 
 
+def unmark_element(element, stream=None):
+    if stream is None:
+        stream = StringIO()
+    if element.text:
+        stream.write(element.text)
+    for sub in element:
+        unmark_element(sub, stream)
+    if element.tail:
+        stream.write(element.tail)
+    return stream.getvalue()
+
+
+# patching Markdown
+Markdown.output_formats["plain"] = unmark_element  # noqa
+__md = Markdown(output_format="plain")  # noqa
+__md.stripTopLevelTags = False
+
+
+def unmark(text):
+    return __md.convert(text)
+
+
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
+        pinned_announcements = Announcement.objects.filter(is_pinned=True)
+        pinned_announcements_condensed = []
+        for ann in pinned_announcements:
+            title = ann.title
+            if len(title) > 15:
+                title = title[:12] + "..."
+            pinned_announcements_condensed.append({
+                "id": ann.pk,
+                "title": title,
+                "content": unmark(ann.content).replace("\n", " "),
+            })
         # Fetch meetings from the last two weeks and the next two weeks
         now = datetime.datetime.now(tz=TAIPEI_TZ)
         two_weeks_ago = (now - datetime.timedelta(weeks=2)).replace(tzinfo=TAIPEI_TZ)
@@ -23,6 +59,7 @@ def index(request):
             request,
             'Panel/index.html',
             {
+                "pinned_announcements": pinned_announcements_condensed,
                 "upcoming_meetings": upcoming_meetings,
                 "past_meetings": past_meetings,
                 "recent_warn_history": recent_warn_history
