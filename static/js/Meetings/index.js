3712bbae516f36ebac3c30e44d73b636b47b3809
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-10-18 10:24:55 +0800

    fix(meeting): 修正「『會議資訊』頁面的『簽到階段』不存在時 JS 會無法運作」的問題
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- static/js/Meetings/index.js
+++ static/js/Meetings/index.js
@@ -2,21 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const signInList = document.getElementById("sign-in-list-content");
     const absentReqList = document.getElementById("absent-request-list-content");
 
-    document.getElementById("sign-in-collapse-button").addEventListener("click", (event) => {
-        if (signInList.getAttribute("expanded")) {  // 隱藏簽到清單
-            signInList.removeAttribute("expanded");
-            event.target.removeAttribute("expanded");
-        } else {  // 顯示簽到清單
-            signInList.setAttribute("expanded", "1");
-            event.target.setAttribute("expanded", "1");
-        }
-    });
+    if (document.getElementById("sign-in-collapse-button") !== null) {  // 檢查簽到階段展開按鈕是否存在
+        document.getElementById("sign-in-collapse-button").addEventListener("click", (event) => {
+            if (signInList.getAttribute("expanded")) {  // 隱藏簽到清單
+                signInList.removeAttribute("expanded");
+                event.target.removeAttribute("expanded");
+            } else {  // 顯示簽到清單
+                signInList.setAttribute("expanded", "1");
+                event.target.setAttribute("expanded", "1");
+            }
+        });
+    }
 
     document.getElementById("absent-request-collapse-button").addEventListener("click", (event) => {
-        if (absentReqList.getAttribute("expanded")) {  // 隱藏簽到清單
+        if (absentReqList.getAttribute("expanded")) {
             absentReqList.removeAttribute("expanded");
             event.target.removeAttribute("expanded");
-        } else {  // 顯示簽到清單
+        } else {
             absentReqList.setAttribute("expanded", "1");
             event.target.setAttribute("expanded", "1");
         }
