598d7dd875f1b84cd36fc0ca7fbda3107e8a717c
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-10-18 15:44:17 +0800

    feat: 更新指令碼加上清除 `web_statics/CACHE` 資料夾內容
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
+rm -R web_static/CACHE
 python3 manage.py compress
 sudo systemctl restart roboweb
