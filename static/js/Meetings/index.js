7317038a70badacc9efe6bd2c5d8d36bbb965044
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-10-18 15:41:55 +0800

    feat(meeting): 為「會議資訊」頁面的「簽到紀錄」區塊加上收合功能
    現在起，「會議資訊」頁面的清單預設皆為收合狀態
    
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- static/js/Meetings/index.js
+++ static/js/Meetings/index.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     const signInList = document.getElementById("sign-in-list-content");
     const absentReqList = document.getElementById("absent-request-list-content");
+    const recordList = document.getElementById("record-list-content");
 
-    if (document.getElementById("sign-in-collapse-button") !== null) {  // 檢查簽到階段展開按鈕是否存在
+    if (signInList !== null) {  // 檢查簽到階段清單是否存在
         document.getElementById("sign-in-collapse-button").addEventListener("click", (event) => {
             if (signInList.getAttribute("expanded")) {  // 隱藏簽到清單
                 signInList.removeAttribute("expanded");
                 event.target.removeAttribute("expanded");
             } else {  // 顯示簽到清單
                 signInList.setAttribute("expanded", "1");
+                event.target.setAttribute("expanded", "1");
+            }
+        });
+    }
+
+    if (recordList !== null) {
+        document.getElementById("record-collapse-button").addEventListener("click", (event) => {
+            if (recordList.getAttribute("expanded")) {  // 隱藏簽到清單
+                recordList.removeAttribute("expanded");
+                event.target.removeAttribute("expanded");
+            } else {  // 顯示簽到清單
+                recordList.setAttribute("expanded", "1");
                 event.target.setAttribute("expanded", "1");
             }
         });
