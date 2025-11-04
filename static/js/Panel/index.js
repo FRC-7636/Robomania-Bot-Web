be905d230a5ee055e87bddffd47755388c9ebcbb
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-11-04 10:46:27 +0800

    feat(announcement): 原先使用 `autoescape off` 注入的公告資料改使用 `json_script` 注入，避免 XSS
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- static/js/Panel/index.js
+++ static/js/Panel/index.js
@@ -20,6 +20,34 @@ window.addEventListener("load", measureHeight);
 window.addEventListener("load", resizeWarnStatus);
 
 document.addEventListener("DOMContentLoaded", () => {
+    const announcements = JSON.parse(document.getElementById("pinned-announcements-data").textContent);
+    console.log(announcements);
+
+    const annText = document.getElementById("announcement-content");
+    const annLink = document.getElementById("announcement-link");
+    // Initial announcement
+    if (announcements.length === 0) {
+        annLink.href = "#";
+        annText.textContent = "(目前沒有釘選中的公告)";
+    } else {
+        annText.textContent = `【${announcements[0].title}】${announcements[0].content}`;
+        annLink.href = `{% url "announcement_info" 0 %}`.replace('/0/', `/${announcements[0].id}/`);
+        let i = 1;
+        if (announcements.length > 1) {
+            setInterval(() => {
+                setTimeout(() => {
+                    annText.style.opacity = "0";
+                }, 500)
+                setTimeout(() => {
+                    annText.textContent = `【${announcements[i].title}】${announcements[i].content}`;
+                    annLink.href = `{% url "announcement_info" 0 %}`.replace('/0/', `/${announcements[i].id}/`);
+                    annText.style.opacity = "1";
+                    i = (i + 1) % announcements.length;
+                }, 1000)
+            }, 10000);
+        }
+    }
+
     const searchParams = new URLSearchParams(window.location.search);
     const syncResult = searchParams.get("sync");
 
@@ -27,6 +55,6 @@ document.addEventListener("DOMContentLoaded", () => {
         alert("頭像同步完成！");
     } else if (syncResult === "failed") {
         alert("頭像同步失敗。\n請確認你登入的 Discord 帳號為你的帳號，並且在授權頁面按下「授權」。");
-        window.history.replaceState({}, document.title, "/");
     }
+    window.history.replaceState({}, document.title, "/");
 });
