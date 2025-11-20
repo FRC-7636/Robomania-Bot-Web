bcaea4fc6ad189c5b5fbf9d90d38a9c6ccbf9f91
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-11-20 23:53:17 +0800

    refactor(uploader): 為一般檔案託管上傳的函式重新命名，以避免與圖片上傳的函式衝突
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Uploader/urls.py
+++ Uploader/urls.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r"^$", views.index, name='upload_index'),
     re_path(r"^$", views.uploader_upload, name='uploader_upload'),
+    path("mdimage/", views.mdimage_upload, name='mdimage_upload'),
 ]
--- Uploader/urls_user_uploads.py
+++ Uploader/urls_user_uploads.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("uploader/<str:uuid>", views.download, name='download'),
     path("uploader/<str:uuid>/", views.uploader_download, name='uploader_download'),
+    path("mdimages/<str:uuid>.png", views.mdimage_download, name='mdimage_download'),
 ]
