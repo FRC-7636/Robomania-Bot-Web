f28045fc6e9fed2ad4733f67d3af448d14283117
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-01-12 22:25:23 +0800

    feat(update): 調整靜態元素資料夾，並新增權限調整指令
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -4,6 +4,9 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-rm -R web_statics/CACHE
+rm -R /var/www/rwb/statics/CACHE
 python3 manage.py compress
+# Set proper permissions (by Gemini)
+sudo find /var/www/rwb/statics/ -type d -exec chmod 755 {} \;
+sudo find /var/www/rwb/statics/ -type f -exec chmod 644 {} \;
 sudo systemctl restart rwb
