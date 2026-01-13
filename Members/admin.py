40050edc7d176e9f5c6c157eb89e39df9405e6e3
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-01-13 18:11:21 +0800

    feat(meeting): 改進 `Members` 於後台面板顯示方式
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Members/admin.py
+++ Members/admin.py
@@ -1,7 +1,20 @@
 # coding=utf-8
 from django.contrib import admin
+
 from .models import DMember, WarningHistory
 
+
 # Register your models here.
-admin.site.register(DMember)
-admin.site.register(WarningHistory)
+@admin.register(DMember)
+class DMemberAdmin(admin.ModelAdmin):
+    list_display = ("id", "discord_id", "real_name", "gen", "warning_points", "email_address", "allow_login")
+    ordering = ("id",)
+    search_fields = ("discord_id", "real_name", "email_address")
+    list_filter = ("gen", "allow_login")
+
+
+@admin.register(WarningHistory)
+class WarningHistoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "member", "points", "reason", "operator", "time")
+    search_fields = ("reason",)
+    list_filter = ("member", "operator", "reason")
