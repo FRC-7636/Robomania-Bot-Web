ac5028a9aac2739df4b805af0d67d57d12e8a8fa
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-01-13 18:24:47 +0800

    feat(meeting): 移除測試用路徑
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Meetings/urls.py
+++ Meetings/urls.py
@@ -13,8 +13,11 @@ urlpatterns = [
     path("<int:meeting_id>/sign_in/<str:sign_in_uuid>/", views.sign_in_view, name="meeting_signin"),
     path("<int:meeting_id>/sign_in/<str:sign_in_uuid>/scan/", views.sign_in_scan_view, name="meeting_signin_scan"),
     path("<int:meeting_id>/submit_absent_request/", views.submit_absent_request, name="meeting_submit_absent"),
-    path("<int:meeting_id>/review_absent_requests/", views.review_absent_requests_page, name="meeting_review_absents"),
-    # path("tws/", views.test_ws, name="websocket_test"),
+    path(
+        "<int:meeting_id>/review_absent_requests/",
+        views.review_absent_requests_page,
+        name="meeting_review_absents"
+    ),
     path(
         "<int:meeting_id>/review_absent_requests_api/",
         views.review_absent_requests_api,
