c6ba898893b2e8da6d704215787864e7f083bfd7
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-01-13 18:24:14 +0800

    feat(meeting): 改進 `Uploader` 於後台面板顯示方式
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- Uploader/admin.py
+++ Uploader/admin.py
@@ -5,5 +5,15 @@ from .models import UserFile, MDImage
 
 
 # Register your models here.
-admin.site.register(UserFile)
-admin.site.register(MDImage)
+@admin.register(UserFile)
+class UserFileAdmin(admin.ModelAdmin):
+    list_display = ("uuid", "name", "mimetype", "uploader", "uploaded_at", "require_login", "require_password")
+    search_fields = ("uuid", "name", "mimetype")
+    list_filter = ("uploader", "require_login", "require_password")
+
+
+@admin.register(MDImage)
+class MDImageAdmin(admin.ModelAdmin):
+    list_display = ("uuid", "uploader", "uploaded_at")
+    search_fields = ("uuid",)
+    list_filter = ("uploader",)
