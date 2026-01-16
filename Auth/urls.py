d09eb96dc5a18359fbf6dc133a2b5591de322c5b
Allen Wei <allen95wei@noreply.codeberg.org>
2026-01-17 00:19:47 +0800

    feat(auth): 停用與 Discord API 相關的路徑
    Signed-off-by: Allen Wei <allen95wei@noreply.codeberg.org>

--- Auth/urls.py
+++ Auth/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path("login/code/", views.code_login_view, name='code_login'),
-    path("login/discord/", views.discord_login_view, name='discord_login'),
+    # path("login/discord/", views.discord_login_view, name='discord_login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
     path("password_change/", views.password_change_view, name='password_change'),
-    path("sync_avatar/", views.sync_avatar_view, name="sync_avatar"),
+    # path("sync_avatar/", views.sync_avatar_view, name="sync_avatar"),
 ]
